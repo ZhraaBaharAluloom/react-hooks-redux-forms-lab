960ec829a9875277fe872ffb8115a73798b23dc7
Zahraa <alwarda@hotmail.co.uk>
2021-08-30 13:35:48 -0700

    write a code in the conteiner component connected to redux

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+//Redux
+import { useDispatch, useSelector } from "react-redux";
+import { bandAdded } from "./bandsSlice";
+
+// Components
+import BandInput from "./BandInput";
+
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const bansName = useSelector((state) => state.bands.entities).map(
+    (_name, index) => <li key={index}>{_name}</li>
+  );
+
+  const dispatch = useDispatch();
+
+  function handleBandSubmit(name) {
+    dispatch(bandAdded(name));
+  }
+
+  return (
+    <>
+      <BandInput onBandSubmit={handleBandSubmit} />
+      <h4>Bands Name:</h4>
+      <ul>{bansName}</ul>
+    </>
+  );
 }
 
 export default BandsContainer;
